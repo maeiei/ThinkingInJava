b414b248487b5d1ac4027254b5a7c3ebb0261de7
maeiei <maeiei@163.com>
2014-08-18 06:50:01 +0800

    3

---
+++ ThinkingInJava/solution/solution/generics/E25_Bounds.java
@@ -0,0 +1,47 @@
+package solution.generics;
+
+interface Low {
+	void c();
+
+	void d();
+}
+
+public class E25_Bounds implements Top, Low {
+
+	public static void main(String[] args) {
+		E25_Bounds bounds = new E25_Bounds();
+		bounds.top(bounds);
+		bounds.low(bounds);
+	}
+
+	public <T extends Top> void top(T t) {
+		t.a();
+		t.b();
+	}
+
+	public <T extends Low> void low(T t) {
+		t.c();
+		t.d();
+	}
+
+	@Override
+	public void c() {
+		System.out.println("Low.c()");
+	}
+
+	@Override
+	public void d() {
+		System.out.println("Low.d()");
+	}
+
+	@Override
+	public void a() {
+		System.out.println("Top.a()");
+	}
+
+	@Override
+	public void b() {
+		System.out.println("Top.b()");
+	}
+
+}
